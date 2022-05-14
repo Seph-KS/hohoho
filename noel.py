91100678f4b628f557ccbd1df5588b61703f945d
Seph-KS <andreysarda@gmail.com>
2022-05-14 15:52:54 -0300

    created the first test with one as parameter

---
+++ noel.py
@@ -0,0 +1,8 @@
+# -*- coding: utf -8 -*-
+'''
+@author: Seph-KS
+'''
+
+
+def solution():
+    pass
---
+++ test_noel.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+'''
+@author: Seph-KS
+'''
+
+import unittest
+from noel import solution
+
+class TestNoel(unittest.TestCase):
+    def test_one_Ho(self):
+        self.assertEqual(solution(1), "Ho!")
+
+unittest.main()
